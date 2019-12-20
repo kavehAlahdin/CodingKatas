0bbf8d43dded7907515f3a4350dfbc9f9f7b6266
Kaveh <kaveh.allahdin@rwth-aachen.de>
2019-12-20 21:22:15 +0100

    update,repitition not considered yet

--- Anagram/Anagram.cs
+++ Anagram/Anagram.cs
@@ -1,10 +1,38 @@
 using System;
 using System.IO;
+using System.Collections.Generic;
 public class Anagram{
-    private const string sourceWord = "documenting";
-    private const string wordListFileName="./worldlist.txt";
+    private static bool isSubSetOfSourceWord(string word,string sourceWord){
+          int index=0;
+          foreach(char character in word)
+            {
+                if(word.Length==0)return true;
+                if(!sourceWord.Contains(character.ToString()))
+                    return false;
+                string newSourceWord=sourceWord.Remove(index,1);
+                string newWord=word.Remove(index,1);
+                index++;
+                return isSubSetOfSourceWord(newWord,newSourceWord);
+                
+            }
+        return true;
+    }
+    private const string wordListFileName="wordlist.txt";
     public static void Main(){
-    StreamReader worldList=File.OpenText(wordListFileName);
+    string sourceWord = "documenting";
+    StreamReader wordList=File.OpenText(wordListFileName);
+    int CandidateNumber=0;
+    string CurrentWord=string.Empty;
+    List<string> wordCandidateList=new List<string>();
+    string word1=string.Empty;
+    string word2=string.Empty;
+    while((CurrentWord= wordList.ReadLine())!=null){
+        if(CurrentWord.Length>=sourceWord.Length)continue;
+        if(!isSubSetOfSourceWord(CurrentWord,sourceWord))continue;        
+        wordCandidateList.Add(CurrentWord);
+        CandidateNumber++;
+        Console.WriteLine("{0}-{1}",CandidateNumber,CurrentWord);
+    }
 
 
     }
